ef233367c372470af4539c35e1ebabd49fdb34e2
Tsega Bogale <133948799+tsegabogale24@users.noreply.github.com>
2025-09-28 06:13:30 +0300

    Update urls.py

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,9 +1,9 @@
-from rest_framework.routers import DefaultRouter
+from rest_framework import routers
 from django.urls import path, include
 from .views import ConversationViewSet, MessageViewSet
 
-# DRF router automatically generates CRUD endpoints for our ViewSets
-router = DefaultRouter()
+# Use routers.DefaultRouter() explicitly
+router = routers.DefaultRouter()
 router.register(r"conversations", ConversationViewSet, basename="conversation")
 router.register(r"messages", MessageViewSet, basename="message")
 
