cd9ac73ce701b9506ea8ff4dc8588d2b9434bb4a
Tsega Bogale <133948799+tsegabogale24@users.noreply.github.com>
2025-09-28 06:21:47 +0300

    Update urls.py

--- messaging_app/urls.py
+++ messaging_app/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include("chats.urls")),  # <-- Must include "api/"
+    path("api/", include("chats.urls")),      # main app routes
+    path("api-auth/", include("rest_framework.urls")),  # <-- required
 ]
