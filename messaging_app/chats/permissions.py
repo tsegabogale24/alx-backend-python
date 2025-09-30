9400e502eb5fb8458265b44c79fc17015539199b
Tsega Bogale <133948799+tsegabogale24@users.noreply.github.com>
2025-09-30 05:13:33 +0300

    Update permissions.py

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -1,5 +1,5 @@
 # messaging_app/chats/permissions.py
-from rest_framework.permissions import permissions
+from rest_framework import permissions
 
 class IsOwner(permissions.BasePermission):
     """
