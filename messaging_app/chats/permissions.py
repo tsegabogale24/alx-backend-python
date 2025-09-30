e4e1d0bfe75e6d0aa5ada2c62f9c92d0a572d531
tsega b <tsegabogale92@gmail.com>
2025-09-30 04:37:22 +0300

    add permissions.py

---
+++ messaging_app/chats/permissions.py
@@ -0,0 +1,10 @@
+# messaging_app/chats/permissions.py
+from rest_framework.permissions import BasePermission
+
+class IsOwner(BasePermission):
+    """
+    Custom permission to only allow users to access their own objects.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        return obj.user == request.user
