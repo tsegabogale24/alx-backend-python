1f172d840faeeb8c6866a70d911c40d062201268
Tsega Bogale <133948799+tsegabogale24@users.noreply.github.com>
2025-09-30 05:30:25 +0300

    Update permissions.py

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -1,10 +1,28 @@
 # messaging_app/chats/permissions.py
-from rest_framework import permissions
+from rest_framework.permissions import BasePermission
 
-class IsOwner(permissions.BasePermission):
+class IsOwner(BasePermission):
     """
     Custom permission to only allow users to access their own objects.
     """
 
     def has_object_permission(self, request, view, obj):
         return obj.user == request.user
+class IsParticipantOfConversation(permissions.BasePermission):
+    """
+    Custom permission:
+    - Only authenticated users can access the API
+    - Only participants of a conversation can send, view, update, or delete messages
+    """
+
+    def has_permission(self, request, view):
+        # Only authenticated users
+        return request.user and request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        """
+        Assumes:
+        - Message model has a ForeignKey to Conversation (obj.conversation)
+        - Conversation model has participants ManyToMany field
+        """
+        return request.user in obj.conversation.participants.all()
