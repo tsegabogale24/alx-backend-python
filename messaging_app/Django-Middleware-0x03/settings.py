b05b866b768399ee101bccca4854b6f26dda066a
Tsega Bogale <133948799+tsegabogale24@users.noreply.github.com>
2025-10-07 22:06:40 +0300

    Update settings.py

--- messaging_app/Django-Middleware-0x03/settings.py
+++ messaging_app/Django-Middleware-0x03/settings.py
@@ -51,6 +51,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    # Custom middleware
+    'chats.middleware.RequestLoggingMiddleware',
 ]
 
 ROOT_URLCONF = 'messaging_app.urls'
